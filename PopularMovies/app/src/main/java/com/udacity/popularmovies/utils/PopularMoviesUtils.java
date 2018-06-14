e8b7d133d667a43718eef47440d0384c970bcbcb
Jason Scott <jason.scott5391@gmail.com>
2018-06-13 22:20:38 -0400

    #21 -> Dynamically determine number of columns based on device size and layout.

--- PopularMovies/app/src/main/java/com/udacity/popularmovies/utils/PopularMoviesUtils.java
+++ PopularMovies/app/src/main/java/com/udacity/popularmovies/utils/PopularMoviesUtils.java
@@ -10,33 +10,39 @@ public class PopularMoviesUtils {
 
     public enum PosterWidth {
 
-        W_342_POSTER_WIDTH("w342"),
-        W_500_POSTER_WIDTH("w500"),
-        W_780_POSTER_WIDTH("w780");
+        W_185_POSTER_WIDTH("w185", 185),
+        W_342_POSTER_WIDTH("w342", 342),
+        W_500_POSTER_WIDTH("w500", 500),
+        W_780_POSTER_WIDTH("w780", 780);
 
-        PosterWidth(String value) {
+        PosterWidth(String key, int value) {
+            this.key = key;
             this.value = value;
         }
 
-        private String value;
+        private String key;
+        private int value;
 
-        private String value() {
+        public String key() {
+            return key;
+        }
+
+        public int value() {
             return value;
         }
     }
 
     public static final PosterWidth DEFAULT_POSTER_WIDTH = PosterWidth.W_500_POSTER_WIDTH;
 
-
     public static void loadNetworkImageIntoView(String imageUrl, ImageView imageView) {
         Picasso.get().load(imageUrl).into(imageView);
     }
 
     public static String buildPosterImageUrl(String posterPath) {
-        return String.format("%s/%s/%s", BASE_MOVIE_DB_URL, DEFAULT_POSTER_WIDTH.value(), posterPath);
+        return String.format("%s/%s/%s", BASE_MOVIE_DB_URL, DEFAULT_POSTER_WIDTH.key(), posterPath);
     }
 
     public static String buildPosterImageUrl(String posterPath, PosterWidth posterWidth) {
-        return String.format("%s/%s/%s", BASE_MOVIE_DB_URL, posterWidth.value(), posterPath);
+        return String.format("%s/%s/%s", BASE_MOVIE_DB_URL, posterWidth.key(), posterPath);
     }
 }
