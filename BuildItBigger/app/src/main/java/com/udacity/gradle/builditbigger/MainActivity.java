1b667d0735135de27f9a916365c69b4e6694e8f1
Jason Scott <jason.scott5391@gmail.com>
2018-10-11 01:09:25 -0400

    #51 -> Created Android library for accepting and displaying a joke.

--- BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,16 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+
+import com.udacity.jokedifferently.JokeDifferently;
+import com.udacity.jokely.JokelyActivity;
+
+import static com.udacity.jokely.JokelyActivity.EXTRA_JOKE_KEY;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +45,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokelyActivity.class);
+        intent.putExtra(EXTRA_JOKE_KEY, JokeDifferently.getAJoke());
+        startActivity(intent);
     }
 
 
