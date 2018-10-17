68ff4a3eaeafea1824babd9d3945f76f3afca010
Jason Scott <jason.scott5391@gmail.com>
2018-10-17 01:17:22 -0400

    #53 -> Implemented endpoint for requesting a joke in GCE and added AsyncTask with progress bar for fetching Joke.

--- BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,24 +1,42 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
-import com.udacity.jokedifferently.JokeDifferently;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 import com.udacity.jokely.JokelyActivity;
+
+import java.io.IOException;
 
 import static com.udacity.jokely.JokelyActivity.EXTRA_JOKE_KEY;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressBar mTellJokeProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTellJokeProgressBar = findViewById(R.id.tell_joke_progress_bar);
+        mTellJokeProgressBar.setVisibility(View.GONE);
     }
 
 
@@ -45,10 +63,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, JokelyActivity.class);
-        intent.putExtra(EXTRA_JOKE_KEY, JokeDifferently.getAJoke());
-        startActivity(intent);
+        new JokeEndpointsAsyncTask(this).execute();
     }
 
 
+    class JokeEndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi mMyApi;
+        private Context mContext;
+
+        public JokeEndpointsAsyncTask(@NonNull Context context) {
+            this.mContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mTellJokeProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            if (mMyApi == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                                request.setDisableGZipContent(true);
+                            }
+                        });
+
+                mMyApi = builder.build();
+            }
+
+            try {
+                return mMyApi.getAJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String joke) {
+            super.onPostExecute(joke);
+            mTellJokeProgressBar.setVisibility(View.GONE);
+            Log.d(TAG, String.format("onPostExecute - joke:%s", joke));
+            Intent intent = new Intent(mContext.getApplicationContext(), JokelyActivity.class);
+            intent.putExtra(EXTRA_JOKE_KEY, joke);
+            startActivity(intent);
+        }
+    }
 }
